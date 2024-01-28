9ed85bca7fd6f4a167969e6993b1c92405b7a539
Mustafah <mustafah.elbanna@gmail.com>
2024-01-28 05:12:16 +0300

    ~

--- src/commands/images.panel.ts
+++ src/commands/images.panel.ts
@@ -6,10 +6,11 @@ export class ImagesPanelViewProvider implements vscode.WebviewViewProvider {
     private _view?: vscode.WebviewView;
 
     constructor(private readonly _extensionUri: vscode.Uri) {
-        // Listen for changes in the active text editor
-        vscode.window.onDidChangeActiveTextEditor(editor => {
-            if (editor && editor.document.languageId === 'dream') {
-                this.updateWebviewContent(editor.document);
+        // Listen for changes in any text document
+        vscode.workspace.onDidChangeTextDocument(event => {
+            const editor = vscode.window.activeTextEditor;
+            if (editor && editor.document === event.document && event.document.languageId === 'dream') {
+                this.updateWebviewContent(event.document);
             }
         });
     }
@@ -18,7 +19,10 @@ export class ImagesPanelViewProvider implements vscode.WebviewViewProvider {
         this._view = webviewView;
         webviewView.webview.options = {
             enableScripts: true,
-            localResourceRoots: [this._extensionUri]
+            localResourceRoots: [
+                this._extensionUri,
+                vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, 'images')
+            ]
         };
         // Initialize with current active editor if it is a .dream file
         const activeEditor = vscode.window.activeTextEditor;
@@ -34,18 +38,6 @@ export class ImagesPanelViewProvider implements vscode.WebviewViewProvider {
         }
     }
 
-    private extractImagePaths2(documentText: string): string[] {
-        const imagePaths = [];
-        const lines = documentText.split('\n');
-        for (const line of lines) {
-            if (line.startsWith('~ images =')) {
-                const paths = line.substring(8).split(',').map(path => `images/${path.trim()}.png`);
-                imagePaths.push(...paths);
-            }
-        }
-        return imagePaths;
-    }
-
 	private extractImagePaths(documentText: string): string[] {
         const imagePaths = [];
         const match = documentText.match(IMAGES_REGEX);
@@ -53,17 +45,21 @@ export class ImagesPanelViewProvider implements vscode.WebviewViewProvider {
             const paths = match[2].split(',').map(path => path.trim());
             const workspaceFolders = vscode.workspace.workspaceFolders;
             if (workspaceFolders) {
-                const workspacePath = workspaceFolders[0].uri.fsPath;
-                return paths.map(path => vscode.Uri.file(path).fsPath);
+                const workspaceFolderUri = vscode.workspace.workspaceFolders[0].uri;
+                return paths.map(path => vscode.Uri.joinPath(workspaceFolderUri, 'images', `${path}.png`).toString());
             }
         }
         return imagePaths;
     }
 
     private getHtmlForWebview(webview: vscode.Webview, imagePaths: string[]): string {
-        const imageTags = imagePaths.map(path => 
-            `<img src="${webview.asWebviewUri(vscode.Uri.file(path))}" />`
-        ).join('');
+
+        const scriptPathOnDisk = vscode.Uri.joinPath(this._extensionUri, 'media', 'masonry.pkgd.min.js');
+        const masonryScriptUri = webview.asWebviewUri(scriptPathOnDisk);
+
+        const imageTags = imagePaths.map(path => `
+        ${path}
+        <img src="${webview.asWebviewUri(vscode.Uri.parse(path))}" /><br/>`).join('');
         return `
             <!DOCTYPE html>
             <html lang="en">
@@ -71,6 +67,7 @@ export class ImagesPanelViewProvider implements vscode.WebviewViewProvider {
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Dreamscript Images</title>
+                <script src="${masonryScriptUri}"></script>
             </head>
             <body>
                 ${imageTags}
